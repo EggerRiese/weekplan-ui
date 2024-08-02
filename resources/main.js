9b245a7a81fd033f0f0d490fc9cda31df60581f6
Joshua Knoop <joshua.voelkerink@gmx.de>
2024-08-02 08:32:28 +0200

    add test data for week tab

--- resources/main.js
+++ resources/main.js
@@ -5,19 +5,39 @@ function changeIds(element, inputId) {
     element.name = element.name?.slice(0, -1) + inputId;
 
     // Recursively process all child elements
-    for (var i = 0; i < element.children.length; i++) {
+    for (let i = 0; i < element.children.length; i++) {
         changeIds(element.children[i], inputId);
     }
 }
 
+function sendPost(data, endpoint) {
+    // Send POST request with JSON data
+    fetch('http://localhost:8080/' + endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-KEY': 'f82222f3-c6b7-4cbf-8343-2df94e10237f'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
 
 const { createApp, ref } = Vue
 
-  createApp({
+createApp({
     data: () => ({
-        tab: 1,
+        tab: 2,
         shoppingList: null,
-        inputId: 1
+        mealList: null,
+        inputId: 1,
+        currentWeek: 0
     }),
 
     setup() {
@@ -31,12 +51,17 @@ const { createApp, ref } = Vue
         switchTab(switchTo) {
             this.tab = switchTo;
 
+            if (this.tab === 1) {
+                this.setWeek();
+                this.fetchMeals();
+            }
+
             if (this.tab === 3) {
                 this.fetchData();
             }
         },
         async fetchData() {
-            this.shoppingList = 
+            this.shoppingList =
             [
                 {
                     "name": "Nudeln",
@@ -55,6 +80,23 @@ const { createApp, ref } = Vue
                 }
             ];
         },
+        async fetchMeals() {
+            this.mealList =
+                [
+                    {
+                        "id": "1",
+                        "name": "Nudeln mit Pesto",
+                    },
+                    {
+                        "id": "2",
+                        "name": "Pfannkuchen",
+                    },
+                    {
+                        "id": "3",
+                        "name": "Mediteraner Auflauf",
+                    }
+                ];
+        },
         createInput() {
             const existingInput = document.getElementById('input-' + this.inputId);
             const newInput = existingInput.cloneNode(true);
@@ -64,15 +106,45 @@ const { createApp, ref } = Vue
             changeIds(newInput, this.inputId);
 
             document.getElementById(existingInput.id).appendChild(newInput);
+        },
+        setWeek() {
+            const today = new Date();
+
+            // Get the first day of the year
+            const startOfYear = new Date(today.getFullYear(), 0, 1);
+
+            // Calculate the number of days between today and the start of the year
+            const pastDaysOfYear = (today - startOfYear) / 86400000;
+
+            // Calculate the current week number
+            this.currentWeek = Math.ceil((pastDaysOfYear + startOfYear.getDay() + 1) / 7);
         }
     }
-  }).mount('#app')
+}).mount('#app')
 
+
+document.getElementById('week-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const formData = new FormData(event.target);
+
+    const data = {
+        week: formData.get('week-number'),
+        monday: formData.get('week-monday'),
+        tuesday: formData.get('week-tuesday'),
+        wednesday: formData.get('week-wednesday'),
+        thursday: formData.get('week-thursday'),
+        friday: formData.get('week-friday'),
+        saturday: formData.get('week-saturday'),
+        sunday: formData.get('week-sunday'),
+    };
+
+    sendPost(data, 'week');
+});
 
 document.getElementById('meal-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
 
-    // Capture form data
     const formData = new FormData(event.target);
 
     // Convert form data to JSON
@@ -91,20 +163,5 @@ document.getElementById('meal-form').addEventListener('submit', function(event) 
         i++;
       }
 
-    // Send POST request with JSON data
-    fetch('http://localhost:8080/meals', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-API-KEY': 'f82222f3-c6b7-4cbf-8343-2df94e10237f'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+      sendPost(data, 'meal');
 });
